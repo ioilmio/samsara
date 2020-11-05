67be319354e98ffbc44f4502f2fc3ed30a451b05
ioilmio <illuminato.salvatore@gmail.com>
2020-11-05 14:40:45 +0100

    webpack server

--- src/background_image.js
+++ src/background_image.js
@@ -4,6 +4,6 @@ export default function createBackgroundImage(source) {
   const box = document.createElement('img');
   // console.log(box);
   box.setAttribute('src', source);
-  createDiv('alert').appendChild(box);
+  createDiv('alerted').appendChild(box);
   return box;
 }
