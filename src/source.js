6252c24c8123940263545f524bf6305edc49f7b5
ioilmio <illuminato.salvatore@gmail.com>
2020-11-10 02:07:55 +0100

    remove commments

--- src/source.js
+++ src/source.js
@@ -1,6 +1,3 @@
-// import '@fortawesome/fontawesome-free/js/fontawesome';
-// import '@fortawesome/fontawesome-free/js/solid';
-// import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import './style.css';
 import navBar from './navbar';
 import mainSection from './mainSection';
@@ -8,30 +5,11 @@ import menuPage from './menuPage';
 import aboutPage from './aboutPage';
 import contactPage from './contactPage';
 
-
-// topMenu.addEventListener('click', togglePage(e) =>
-//   console.log(e.target))
-//   ;
-
-
 function init() {
-  // const navbar =
   navBar();
-  // const main =
   mainSection();
-  // const menu =
-  // menuPage();
-  // document.body.appendChild(main);
-  // const about =
   aboutPage();
-  // const contact =
-  // contactPage();
-  // document.body.appendChild(navbar);
-  // document.main.appendChild(menu);
   const main = document.getElementById('main');
-  // main.appendChild(menu);
-  // document.main.appendChild(about);
-  // document.main.appendChild(contact);
   const topMenu = document.getElementById('top-menu');
   topMenu.addEventListener('click', (e) => {
     if (e.target.id === 'menuTab') {
